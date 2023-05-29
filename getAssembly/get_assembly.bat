a9cef83357785c2969bae43bea84fc345bba6654
cyrsztof <cyrsztof@gmail.com>
2023-05-29 18:25:24 +0200

    Add files via upload

--- getAssembly/get_assembly.bat
+++ getAssembly/get_assembly.bat
@@ -3,21 +3,23 @@ setlocal enabledelayedexpansion
 
 set "input_file=names.csv"
 set "output_directory=output"
-set "input_directory=r007ue"
+set "input_directory=..\..\..\cyberpunk\r007ue"
 
 if not exist "%output_directory%" mkdir "%output_directory%"
 
-set "invalid_chars=<>:""/\|?*"
+set index=1
 
 for /f "usebackq delims=" %%a in ("%input_file%") do (
 	set "name=%%a"
 	
-	rem Sanitize the name by replacing invalid characters with an underscore (_)
-    set "sanitized_name=!name!"
-
-    for %%b in (!invalid_chars!) do set "sanitized_name=!sanitized_name:%%b=_!"
-    
-    vtune -report hotspots -r %input_directory% -source-object function=!name! -group-by=address -column=Assembly,Instructions,Clockticks -report-output "%output_directory%\!sanitized_name!_results.csv" -format csv -csv delimiter ;
+	rem Generate the output file name using a numerical index
+    set "output_filename=%output_directory%\output_!index!.csv"
+    set /a index+=1
+	
+    vtune -report hotspots -r !input_directory! -source-object function=!name! -group-by=address -column=Assembly,Instructions,Clockticks -report-output "!output_filename!" -format csv -csv-delimiter ;
+	
+	rem Remove the header rows from the output file
+    more +2 "!output_filename!" > "!output_filename!.tmp" & move /y "!output_filename!.tmp" "!output_filename!" > nul
 )
 
 echo Execution completed. Generating merged output CSV file.
