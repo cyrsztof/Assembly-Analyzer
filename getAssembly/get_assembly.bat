fe6bb28e00669744864df53b1cfd1bb37c8d6dff
cyrsztof <cyrsztof@gmail.com>
2023-05-30 19:42:28 +0200

    Update get_assembly.bat

--- getAssembly/get_assembly.bat
+++ getAssembly/get_assembly.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 set "input_file=names.csv"
 set "output_directory=output"
-set "input_directory=..\..\..\cyberpunk\r007ue"
+set "input_directory=r000ue2"
 
 if not exist "%output_directory%" mkdir "%output_directory%"
 
@@ -16,7 +16,7 @@ for /f "usebackq delims=" %%a in ("%input_file%") do (
     set "output_filename=%output_directory%\output_!index!.csv"
     set /a index+=1
 	
-    vtune -report hotspots -r !input_directory! -source-object function=!name! -group-by=address -column=Assembly,Instructions,Clockticks -report-output "!output_filename!" -format csv -csv-delimiter ;
+    vtune -report hotspots -r !input_directory! -source-object function=!name! -group-by=address -column=Assembly,"Hardware Event Sample Count:INST_RETIRED.ANY:Self","Hardware Event Sample Count:CPU_CLK_UNHALTED.THREAD:Self" -show-as samples -report-output "!output_filename!" -format csv -csv-delimiter ;
 	
 	rem Remove the header rows from the output file
     more +2 "!output_filename!" > "!output_filename!.tmp" & move /y "!output_filename!.tmp" "!output_filename!" > nul
@@ -27,6 +27,5 @@ echo Execution completed. Generating merged output CSV file.
 cd "%output_directory%"
 
 copy *.csv merged_output.csv
-del /Q *.csv
 
 echo Merged output CSV file generated successfully.
